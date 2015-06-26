4dd33a5fa6b455c497cd5326527611d076ef7fd5
Davis Koh <koh.davis.0@gmail.com>
2015-06-26 16:46:38 -0400

    add sleep

--- BigNerdRanch/ClassCertificates/main.m
+++ BigNerdRanch/ClassCertificates/main.m
@@ -1,4 +1,5 @@
 #include <stdio.h>
+#include <unistd.h>
 
 void congratulateStudent(char *student, char *course, int numDays) {
     printf("%s has done as much %s Programming as I could fit into %d days.\n", student, course, numDays);
@@ -6,6 +7,7 @@ void congratulateStudent(char *student, char *course, int numDays) {
 
 int main(int argc, const char *argv[]) {
     congratulateStudent("Mark", "Cocoa", 5);
+    sleep(2);
     congratulateStudent("Bo", "Objective-C", 2);
 
     return 0;
