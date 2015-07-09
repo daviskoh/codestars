f77d042a2d0f7afe2afc2313127ab9f893d97931
daviskoh <koh.davis.0@gmail.com>
2015-07-09 16:16:42 -0400

    redo BMITime using xcode

---
+++ BigNerdRanch/objective-c/BMITime/BMITime/main.m
@@ -0,0 +1,17 @@
+//
+//  main.m
+//  BMITime
+//
+//  Created by Davis Koh on 7/9/15.
+//  Copyright (c) 2015 com.DavisKoh. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+int main(int argc, const char * argv[]) {
+    @autoreleasepool {
+        // insert code here...
+        NSLog(@"Hello, World!");
+    }
+    return 0;
+}
--- BigNerdRanch/objective-c/BMITime/Person.m
+++ BigNerdRanch/objective-c/BMITime/Person.m
@@ -1,3 +1,11 @@
+//
+//  Person.m
+//  BMITime
+//
+//  Created by Davis Koh on 7/9/15.
+//  Copyright (c) 2015 com.DavisKoh. All rights reserved.
+//
+
 #import "Person.h"
 
 @interface Person ()
@@ -11,12 +19,12 @@
 
 - (id)initWithHeightInMeters:(float)height weightInKilos:(int)weight {
     self = [super init];
-
+    
     if (self) {
-        _heightInMeters = height;
-        _weightInKilos = weight;
+        self.heightInMeters = height;
+        self.weightInKilos = weight;
     }
-
+    
     return self;
 }
 
