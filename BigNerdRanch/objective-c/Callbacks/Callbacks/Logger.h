075ca29d1562ce84041b61754d47ccb8c6838b8a
daviskoh <koh.davis.0@gmail.com>
2015-07-11 17:39:02 -0400

    Target Action and Helper Object examples

---
+++ BigNerdRanch/objective-c/Callbacks/Callbacks/Logger.h
@@ -0,0 +1,18 @@
+//
+//  Logger.h
+//  Callbacks
+//
+//  Created by Davis Koh on 7/11/15.
+//  Copyright © 2015 com.DavisKoh. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+@interface Logger : NSObject {
+    NSMutableData *incomingData;
+}
+
+// t - sender of message
+- (void)sayOuch:(NSTimer *)t;
+
+@end
---
+++ BigNerdRanch/objective-c/Callbacks/Callbacks/main.m
@@ -0,0 +1,44 @@
+//
+//  main.m
+//  Callbacks
+//
+//  Created by Davis Koh on 7/11/15.
+//  Copyright © 2015 com.DavisKoh. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+#import "Logger.h"
+
+/**
+ * Rules for Callbacks:
+ *
+ * when sending 1 callback to 1 object, use Target-Action
+ * - ex: timer below
+ *
+ * when sending multiple callbacks to 1 object, use Helper Object w/ Protocol
+ * - ex: fetchConn below
+ */
+
+int main(int argc, const char * argv[]) {
+    @autoreleasepool {
+        Logger *logger = [[Logger alloc] init];
+        
+        NSURL *url = [NSURL URLWithString:@"http://www.gutenberg.org/cache/epub/205/pg205.txt"];
+        
+        NSURLRequest *request = [NSURLRequest requestWithURL:url];
+        
+        __unused NSURLConnection *fetchConn = [[NSURLConnection alloc] initWithRequest:request
+                                                                              delegate:logger
+                                                                      startImmediately:YES];
+        
+        __unused NSTimer *timer = [NSTimer scheduledTimerWithTimeInterval:2.0
+                                                            target:logger
+                                                        selector:@selector(sayOuch:)
+                                                        userInfo:(nil)
+                                                         repeats:YES];
+        
+        // run loop is an infinite loop waiting for something to happen
+        [[NSRunLoop currentRunLoop] run];
+    }
+    return 0;
+}
