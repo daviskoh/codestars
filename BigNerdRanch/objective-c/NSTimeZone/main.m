d4079c87941a29cf062238e65af52ffd42e3e965
daviskoh <koh.davis.0@gmail.com>
2015-07-12 09:34:16 -0400

    move big nerd ranch over to concepts practice

--- BigNerdRanch/objective-c/NSTimeZone/main.m
+++
@@ -1,9 +0,0 @@
-#import <Foundation/Foundation.h>
-
-int main(int charc, const char *argv[]) {
-    @autoreleasepool {
-        NSTimeZone *timeZone = [NSTimeZone systemTimeZone];
-        NSLog(@"It is currently %s daylight savings time.", [timeZone isDaylightSavingTime] ? "NOT" : "");
-    }
-    return 0;
-}
